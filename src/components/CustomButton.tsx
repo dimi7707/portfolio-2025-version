75944f4356ad2ab5d92a52aa25d5f4d6dd09baaa
Dimitri Avila <dimitriavila@192.168.0.5>
2025-03-22 10:13:47 -0500

    chore: installed TS and react

---
+++ src/components/CustomButton.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../styles/global.css'
+
+export default function CustomButton() {
+  return (
+    <div>
+      <button>Hola soy un botón feliz :) </button>
+    </div>
+  )
+}
---
+++ src/components/TestComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import CustomButton from './CustomButton'
+
+export default function TestComponent() {
+  return (
+    <div>
+      <h3>Hola soy un componente de react :D</h3>
+      <CustomButton />
+    </div>
+  )
+}
