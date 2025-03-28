1e71a09aaee1026f10475fcc83e1b3e819b88fb2
Dimitri Avila <dimitriavila@MacBook-Air-de-Dimitri.local>
2025-03-28 12:17:51 -0500

    chore: added css pre-processor and testing css modules

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "../styles/global.css";
+import styles from "./CustomButton.module.scss";
 
 export default function CustomButton() {
   return (
     <div>
-      <button type="button">Hola soy un botón feliz :) </button>
+      <button
+        type="button"
+        className={styles.button}
+      >
+          Hola soy un botón feliz :)
+      </button>
     </div>
   );
 }
